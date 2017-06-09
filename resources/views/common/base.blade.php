4492cb93a9f18e3c77f0d5a79d4bd3dfa3849c50
Dennis van der Veeke <dienns@dyescape.com>
2017-06-10 00:39:27 +0200

    fixed shit

---
+++ resources/views/common/base.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>AdminLTE 2 | Dashboard</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <!-- Bootstrap 3.3.2 -->
+        <link href="{{ asset("/bower_components/AdminLTE/bootstrap/css/bootstrap.css") }}" rel="stylesheet" type="text/css" />
+        <!-- Font Awesome Icons -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <!-- Theme style -->
+        <link href="{{ asset("/bower_components/AdminLTE/dist/css/AdminLTE.css")}}" rel="stylesheet" type="text/css" />
+        <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+              page. However, you can choose any other skin. Make sure you
+              apply the skin class to the body tag so the changes take effect.
+        -->
+        <link href="{{ asset("/bower_components/AdminLTE/dist/css/skins/skin-red.css")}}" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="skin-red">
+
+        @yield("base_content")
+
+        @yield("models")
+
+        <!-- jQuery 2.1.3 -->
+        <script src="{{ asset ("/bower_components/AdminLTE/plugins/jQuery/jQuery-2.2.3.min.js") }}"></script>
+        <!-- Bootstrap 3.3.2 JS -->
+        <script src="{{ asset ("/bower_components/AdminLTE/bootstrap/js/bootstrap.js") }}" type="text/javascript"></script>
+        <!-- AdminLTE App -->
+        <script src="{{ asset ("/bower_components/AdminLTE/dist/js/app.min.js") }}" type="text/javascript"></script>
+
+        @yield("scripts")
+
+    </body>
+</html>
