f01514cb39e23f54d4d5dca42482be768fba9bf2
Thom van heeswijk <Thom@icreate.nl>
2017-06-10 23:29:32 +0200

    We got a 10

--- resources/services/ABN/AbnService.php
+++ resources/services/ABN/AbnService.php
@@ -27,12 +27,16 @@ class AbnService
      * Gets Transactions
      *
      * @param  array $token
-     * @param  null $accountNumber
+     * @param  array $data
      * @return mixed
      */
 
-    public function getTransactions(array $token, $accountNumber = null, $dateFrom, $dateTo )
+    public function getTransactions(array $token, array $data )
     {
+        $accountNumber = $data['accountNumber'];
+        $dateFrom      = $data['dataForm'];
+        $dateTo        = $data['dateTo'];
+
         if(!$accountNumber)$accountNumber = env('ABN_ACCOUNT_NUMBER');
         $next = true;
         $nextPage = Null;
@@ -51,7 +55,7 @@ class AbnService
                 $url = "https://api-sandbox.abnamro.com/v1/ais/transactions?accountNumber=".$accountNumber."&bookDateFrom='.$dateFrom.'&bookDateTo='.$dateTo.'&nextPageKey=".$nextPage;
             }
 
-           $json = $this->curl('GET', $url, ["Authorization: Bearer ".$token['access_token']]);
+           $json = $this->curl('GET', ['url'=>$url, 'options'=> "Authorization: Bearer ".$token['access_token']]);
 
             $json = explode('{', $json);
             $json[0] = "";
@@ -79,7 +83,7 @@ class AbnService
     {
         if(!$accountNumber)$accountNumber = env('ABN_ACCOUNT_NUMBER');
 
-        $json = $this->curl('GET',"https://api-sandbox.abnamro.com/v1/ais/accountbalances?accountNumber=".$accountNumber,["Authorization: Bearer ".$token['access_token']]);
+        $json = $this->curl('GET',['url'=>"https://api-sandbox.abnamro.com/v1/ais/accountbalances?accountNumber=".$accountNumber,'options' =>["Authorization: Bearer ".$token['access_token']]]);
 
         $json = explode('{', $json);
         $json[0] = "";
@@ -100,7 +104,7 @@ class AbnService
     {
         if(!$accountNumber)$accountNumber = env('ABN_ACCOUNT_NUMBER');
 
-        $json = $this->curl('GET',"https://api-sandbox.abnamro.com/v1/ais/accounts/".$accountNumber,["Authorization: Bearer ".$token['access_token']] );
+        $json = $this->curl('GET',['url' => "https://api-sandbox.abnamro.com/v1/ais/accounts/".$accountNumber,'options' =>["Authorization: Bearer ".$token['access_token']]] );
 
         $json = explode('{', $json);
         $json[0] = "";
@@ -113,14 +117,15 @@ class AbnService
      * Build the CRUL and does the crul requests
      *
      * @param $type
-     * @param $url
-     * @param $options
-     * @param null $data
+     * @param array $parameter
      * @return mixed
      */
 
-    private function curl($type, $url, $options, $data = null)
+    private function curl($type, array $parameter)
     {
+        $url     = $parameter['url'];
+        $options = $parameter['options'];
+
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_FRESH_CONNECT,1);
@@ -133,6 +138,8 @@ class AbnService
         }
         else
         {
+            $data = $parameter['data'];
+
             curl_setopt($curl, CURLOPT_POSTFIELDS,$data);
             curl_setopt($curl, CURLOPT_POST, 1);
         }
