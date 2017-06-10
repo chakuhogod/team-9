301b6700f21f48aade57f7b6942e9c9a978316fc
Dennis van der Veeke <dienns@dyescape.com>
2017-06-10 21:19:43 +0200

    twan you take the wheel

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -14,7 +14,40 @@
         <!-- Main content -->
         <section class="content">
 
-            <!-- Your Page Content Here -->
+            <div class="container content-container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="search-filters">
+                            <form id="searchForm" action="#" method="POST" class="sidebar-form">
+                                <div class="input-group">
+                                    <input name="beginPeriod" id="beginPeriod" class="form-control" type="date">
+                                    <span class="input-group-btn">
+                                        <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="search-filters">
+                            <form id="searchForm" action="#" method="POST" class="sidebar-form">
+                                <div class="input-group">
+                                    <input name="endPeriod" id="endPeriod" class="form-control" type="date">
+                                    <span class="input-group-btn">
+                                        <button type="submit" name="search" id="search-btn" class="btn btn-flat">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
 
         </section><!-- /.content -->
     </div><!-- /.content-wrapper -->
