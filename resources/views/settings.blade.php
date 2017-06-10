62e1908d8ef2c66a2b90acfb2ce740b57423b42e
Dennis van der Veeke <dienns@dyescape.com>
2017-06-10 15:56:49 +0200

    removed some bullshit

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -9,10 +9,6 @@
             <h1>
                 Settings
             </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
-            </ol>
         </section>
 
         <!-- Main content -->
