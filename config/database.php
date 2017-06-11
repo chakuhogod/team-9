00a7add318b14327a75e9da90e78323d95fd7f40
Dennis van der Veeke <dienns@dyescape.com>
2017-06-11 12:30:47 +0200

    Create database.php

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '50278'),
             'database' => env('DB_DATABASE', 'banklinq'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'password'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
