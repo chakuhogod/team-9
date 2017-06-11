5f3bcfb5ad1f0847b5a3425862f67fd45752c3a2
DANIELPC\Hackathon <danielm12@hotmail.nl>
2017-06-11 09:20:40 +0200

    Added Sendback

--- app/Http/Controllers/ExactBack.php
+++ app/Http/Controllers/ExactBack.php
@@ -9,9 +9,11 @@ class ExactBack extends BaseController
 {
     public function GetData(ExactRepository $exact) {
 
+        $sendback = file_get_contents("../app/Repositories/ExactSendBack.csv");
         $sales = file_get_contents("../app/Repositories/ExactSales.csv");
         $purchase = file_get_contents("../app/Repositories/ExactPurchase.csv");
 
+        file_put_contents("../app/Repositories/ExactSendBack.csv","");
         file_put_contents("../app/Repositories/ExactSales.csv", "");
         file_put_contents("../app/Repositories/ExactPurchase.csv", "");
 
@@ -22,6 +24,11 @@ class ExactBack extends BaseController
         if (!empty($purchase)) {
             echo $purchase;
             $exact->CreatePurchaseEntry($purchase);
+        }
+
+        if (!empty($sendback)){
+            header("Location: ".$sendback);
+            exit;
         }
 
         return view('ExactBack', ['test' => 'Exact Online with Laravel!']);
